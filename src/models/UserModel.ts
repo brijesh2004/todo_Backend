993ffe00f97e344bb31dd29a2f68de3eaff67b50
brijesh2004 <brskumar0102@gmail.com>
2024-05-10 09:19:54 +0530

    Todo Server

---
+++ src/models/UserModel.ts
@@ -0,0 +1,33 @@
+
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username:
+    {
+        type: String,
+        required: true
+    },
+    email:
+    {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:
+    {
+        type: String,
+        required: true
+    },
+    todo:[{
+        todos:{
+            type:String,
+        }
+    }],
+    tokens:{
+        type:String,
+    }
+});
+
+const User = mongoose.model('User' , userSchema);
+
+module.exports = {User};
